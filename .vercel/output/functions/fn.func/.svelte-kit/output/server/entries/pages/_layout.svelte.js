cc6abed42f91fad14e597a17f10743764e13e074
eur2 <gregory.dapra@gmail.com>
2023-12-20 15:16:13 +0100

    rm vercel folder

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/entries/pages/_layout.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr.js";
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `${$$result.head += `<!-- HEAD_svelte-1jw7t2c_START -->${$$result.title = `<title>Projets: plateforme de publication des concours et consultations d’architecture</title>`, ""}<!-- HEAD_svelte-1jw7t2c_END -->`, ""} ${slots.default ? slots.default({}) : ``}`;
-});
-export {
-  Layout as default
-};
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1._dhCp_4B.js","_app/immutable/chunks/scheduler.Sga31Z68.js","_app/immutable/chunks/index.5_Pvhs1g.js","_app/immutable/chunks/singletons.tx2v6ZCy.js","_app/immutable/chunks/index.syzS8QXI.js"];
-export const stylesheets = [];
-export const fonts = [];
